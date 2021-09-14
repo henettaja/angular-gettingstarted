fae14d524880c6943255942b29f45ba4c06562ec
Henri Väisänen <henri.vaisane@gmail.com>
2021-09-14 13:04:44 +0300

    Module 9 done

--- APM-Start/src/app/products/product-list.component.ts
+++ APM-Start/src/app/products/product-list.component.ts
@@ -1,43 +1,54 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from "@angular/core"
+import { IProduct } from "./product"
+import { ProductService } from "./product.service"
 
 @Component({
-    selector: 'pm-products',
-    templateUrl: './product-list.component.html'
+    selector: "pm-products",
+    templateUrl: "./product-list.component.html",
+    styleUrls: ["./product-list.component.css"]
 })
 
-export class ProductListComponent {
-    pageTitle: string = "Product list"
+export class ProductListComponent implements OnInit {
 
-    products: any[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2021",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-          },
-          {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2021",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-          },
-          {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2021",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-          },
-    ]
+    constructor(private productService: ProductService) {}
+
+    pageTitle = "Product list"
+    showImage = false
+    imageWidth = 50
+    imageMargin = 2
+
+    private _listFilter = ""
+
+    get listFilter(): string {
+        return this._listFilter
+    }
+    set listFilter(value: string) {
+        this._listFilter = value
+        console.log("In setter: ", value)
+        this.filteredProducts = this.performFilter(value)
+    }
+
+    products: IProduct[] = []
+
+    filteredProducts: IProduct[] = []
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase()
+        return this.products.filter((product: IProduct) => 
+            product.productName.toLocaleLowerCase().includes(filterBy))
+    }
+
+    toggleImage(): void {
+        this.showImage = !this.showImage
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = "Product list: " + message
+    }
+    
+    ngOnInit(): void {
+        this.products = this.productService.getProducts()
+        this.filteredProducts = this.products
+    }
+
 }
